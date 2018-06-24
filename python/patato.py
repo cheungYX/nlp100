c2803da71e66004eaa19a5dcb8c1c06a6ad10a7d
cheungYX <cheunggiven@icloud.com>
2018-06-24 12:19:01 +0900

    update

---
+++ python/patato.py
@@ -0,0 +1,8 @@
+target = "パタトクカシーー"
+
+res = ""
+
+for i in range(1, len(target) - 1, 2):
+	res += target[i]
+
+print(res)
---
+++ python/patoka_taxi.py
@@ -0,0 +1,10 @@
+word1 = "パトカー"
+word2 = "タクシー"
+
+res = ""
+
+for i,j in zip(range(0, len(word1)), range(0,len(word2))):
+	res += word1[i]
+	res += word2[j]
+
+print(res)
