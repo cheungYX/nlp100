c56c9d68eb3be9658fcb8f016cbc3eea2327fecb
cheungYX <cheunggiven@icloud.com>
2018-06-24 11:53:55 +0900

    first commit

---
+++ python/reverse.py
@@ -0,0 +1,13 @@
+target = "stressed"
+
+def reverse(target):
+    if len(target) == 0 or target is None:
+        return
+    res = ""
+
+    for i in range(len(target) - 1, -1, -1):
+      res += target[i]
+
+    print(res)
+
+reverse(target)
